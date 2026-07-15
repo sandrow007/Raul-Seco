d8a3ede9359f0c9e0fbe19c2ac5a98aed6905f68
Sandro <sandro.a007@gmail.com>
2026-07-15 21:56:05 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+window.addEventListener('DOMContentLoaded', function() {
+    if (!localStorage.getItem('cookiesAccepted')) {
+        document.getElementById('cookie-banner-container').classList.add('visible');
+    }
+});
+
+function acceptCookies() {
+    localStorage.setItem('cookiesAccepted', 'true');
+    document.getElementById('cookie-banner-container').classList.remove('visible');
+}
+
+function denyCookies() {
+    document.getElementById('cookie-banner-container').classList.remove('visible');
+}
